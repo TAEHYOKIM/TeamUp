977bc11d4c2b7b830e284afe72d1a999ca4f6a64
INGYEOMENTAL <37355339+TAEHYOKIM@users.noreply.github.com>
2018-04-07 13:30:29 +0900

    수정

--- TeamUp.py
+++ TeamUp.py
@@ -4,7 +4,6 @@ Created on Wed Mar 28 22:29:39 2018
 """
 # 이제 여기에다 코드를 써내려(복붙) 가야지
 
-#%%(1)
 
 import pandas as pd
 from pandas import Series, DataFrame
@@ -12,7 +11,6 @@ from pandas import Series, DataFrame
 art = pd.read_csv('/Users/hbk/github/TeamUp/크롤링 데이터셋/art.csv')
 art
 art.columns = ['idx','제목','삭제','내용']
-art.head()
 art = art[['제목','내용']]
 art.head()
 
@@ -21,16 +19,27 @@ t = Twitter()
 
 res = {}
 num = 0
+df = DataFrame()
+
 for i in art['제목'].values:
-    txt = t.pos(i)
-    lst = []
-    for j in txt:
-        if j[1] == 'Noun':
-            lst.append(j[0])
-    res[num] = lst
+    #txt = t.pos(i)
+    txt = t.morphs(i)
+    res[num] = txt
     num += 1
-        
+res
+df = DataFrame(Series(res), columns = ['art'])
+df
+
+
+DataFrame([res.values()])
+#    for j in txt:
+#        if j[1] == 'Noun':
+#            lst.append(j[0])
 res  # dict
 df = DataFrame(Series(res))
 df.columns = ['art']
 df
+
+t.morphs('아버지가 방에 들어가신다.')  # 형태소 
+t.nouns('아버지가 방에 들어가신다.')
+t.pos('아버지가 방에 들어가신다.') # 형태소(세부적)
